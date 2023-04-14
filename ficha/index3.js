d0592fa8f4f2f9496816a1c7dc9c1e185fa37fcc
Alejo6648 <alorgomez2006@gmail.com>
2023-04-14 02:30:52 -0500

    cambios

---
+++ ficha/index3.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form");
+const nombre = document.getElementById("nombre_producto");
+const cliente = document.getElementById("nombre_cliente");
+const versiones = document.getElementById("versiones");
+const versionactual = document.getElementById("version");
+const modulo = document.getElementById("modulo");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (nombre.value === "" || cliente.value === "" || versiones.value === "" || versionactual.value === "" || modulo.value === "") {
+    alert("Por favor, complete todos los campos.");
+  } else {
+    alert("Formulario enviado correctamente.");
+    form.reset();
+  }
+});
